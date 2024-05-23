ab09573bd8f202138d348c134999bc5845f8bc1f
OSSIRISSA <pipcovich003@gmail.com>
2024-05-23 19:02:09 +0300

    - Basic Character Movement

--- src/main/java/labirinthan/Level0.java
+++ src/main/java/labirinthan/Level0.java
@@ -3,46 +3,55 @@ package labirinthan;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.AppStateManager;
-import com.jme3.asset.AssetManager;
-import com.jme3.light.AmbientLight;
+import com.jme3.bullet.BulletAppState;
 import com.jme3.light.DirectionalLight;
-import com.jme3.light.Light;
-import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
 
 public class Level0 extends Level {
-    public Level0(SimpleApplication application) {
+
+    private BulletAppState bulletAppState;
+
+    public Level0(SimpleApplication application, BulletAppState bulletAppState) {
         super(application, "Level0");
+        this.bulletAppState = bulletAppState;
     }
 
     @Override
     public void initialize(AppStateManager sm, Application application) {
         super.initialize(sm, application);
-        walls.add(new Wall((wallWidth*6+passageWidth*5), wallHeight, wallWidth, assetManager, localRootNode, (wallWidth*6+passageWidth*5)/2, wallHeight/2, wallWidth/2));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*3+passageWidth*2, assetManager, localRootNode, wallWidth/2, wallHeight/2, (wallWidth*3+passageWidth*2)/2));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*2+passageWidth*1, assetManager, localRootNode, wallWidth*3.5f+passageWidth*3, wallHeight/2, (wallWidth*2+passageWidth*1)/2));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*3+passageWidth*2, assetManager, localRootNode, wallWidth*5.5f+passageWidth*5, wallHeight/2, (wallWidth*3+passageWidth*2)/2));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*3+passageWidth*2, assetManager, localRootNode, wallWidth*5.5f+passageWidth*5, wallHeight/2, wallWidth*4.5f+passageWidth*4));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*3+passageWidth*2, assetManager, localRootNode, wallWidth/2, wallHeight/2, wallWidth*4.5f+passageWidth*4));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*2+passageWidth, assetManager, localRootNode, wallWidth*1.5f+passageWidth, wallHeight/2, wallWidth*2+passageWidth*1.5f));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*4+passageWidth*3, assetManager, localRootNode, wallWidth*4.5f+passageWidth*4, wallHeight/2, wallWidth*3+passageWidth*2.5f));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*2+passageWidth, assetManager, localRootNode, wallWidth*1.5f+passageWidth, wallHeight/2, wallWidth*4+passageWidth*3.5f));
-        walls.add(new Wall(wallWidth, wallHeight, wallWidth*2+passageWidth, assetManager, localRootNode, wallWidth*3.5f+passageWidth*3, wallHeight/2, wallWidth*3+passageWidth*2.5f));
-        walls.add(new Wall((wallWidth*6+passageWidth*5), wallHeight, wallWidth, assetManager, localRootNode, (wallWidth*6+passageWidth*5)/2, wallHeight/2, wallWidth*5.5f+passageWidth*5));
-        walls.add(new Wall(wallWidth*3+passageWidth*2, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*2.5f+passageWidth*2, wallHeight/2, wallWidth*1.5f+passageWidth*1));
-        walls.add(new Wall(wallWidth*4+passageWidth*3, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*3+passageWidth*2.5f, wallHeight/2, wallWidth*2.5f+passageWidth*2));
-        walls.add(new Wall(wallWidth*3+passageWidth*2, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*2.5f+passageWidth*2, wallHeight/2, wallWidth*1.5f+passageWidth*1));
-        walls.add(new Wall(wallWidth*2+passageWidth*1, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*1+passageWidth*0.5f, wallHeight/2, wallWidth*3.5f+passageWidth*3));
-        walls.add(new Wall(wallWidth*2+passageWidth*1, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*3+passageWidth*2.5f, wallHeight/2, wallWidth*3.5f+passageWidth*3));
-        walls.add(new Wall(wallWidth*2+passageWidth*1, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*2+passageWidth*1.5f, wallHeight/2, wallWidth*4.5f+passageWidth*4));
-        walls.add(new Wall(wallWidth*2+passageWidth*1, wallHeight, wallWidth, assetManager, localRootNode, wallWidth*4+passageWidth*3.5f, wallHeight/2, wallWidth*4.5f+passageWidth*4));
-        floor = new Floor(50, 0, 50, assetManager, localRootNode, 0,0,0);
+
+        // Adding walls
+        addWall((wallWidth * 6 + passageWidth * 5), wallHeight, wallWidth, (wallWidth * 6 + passageWidth * 5) / 2, wallHeight / 2, wallWidth / 2);
+        addWall(wallWidth, wallHeight, wallWidth * 3 + passageWidth * 2, wallWidth / 2, wallHeight / 2, (wallWidth * 3 + passageWidth * 2) / 2);
+        addWall(wallWidth, wallHeight, wallWidth * 2 + passageWidth, wallWidth * 3.5f + passageWidth * 3, wallHeight / 2, (wallWidth * 2 + passageWidth) / 2);
+        addWall(wallWidth, wallHeight, wallWidth * 3 + passageWidth * 2, wallWidth * 5.5f + passageWidth * 5, wallHeight / 2, (wallWidth * 3 + passageWidth * 2) / 2);
+        addWall(wallWidth, wallHeight, wallWidth * 3 + passageWidth * 2, wallWidth * 5.5f + passageWidth * 5, wallHeight / 2, wallWidth * 4.5f + passageWidth * 4);
+        addWall(wallWidth, wallHeight, wallWidth * 3 + passageWidth * 2, wallWidth / 2, wallHeight / 2, wallWidth * 4.5f + passageWidth * 4);
+        addWall(wallWidth, wallHeight, wallWidth * 2 + passageWidth, wallWidth * 1.5f + passageWidth, wallHeight / 2, wallWidth * 2 + passageWidth * 1.5f);
+        addWall(wallWidth, wallHeight, wallWidth * 4 + passageWidth * 3, wallWidth * 4.5f + passageWidth * 4, wallHeight / 2, wallWidth * 3 + passageWidth * 2.5f);
+        addWall(wallWidth, wallHeight, wallWidth * 2 + passageWidth, wallWidth * 1.5f + passageWidth, wallHeight / 2, wallWidth * 4 + passageWidth * 3.5f);
+        addWall(wallWidth, wallHeight, wallWidth * 2 + passageWidth, wallWidth * 3.5f + passageWidth * 3, wallHeight / 2, wallWidth * 3 + passageWidth * 2.5f);
+        addWall((wallWidth * 6 + passageWidth * 5), wallHeight, wallWidth, (wallWidth * 6 + passageWidth * 5) / 2, wallHeight / 2, wallWidth * 5.5f + passageWidth * 5);
+        addWall(wallWidth * 3 + passageWidth * 2, wallHeight, wallWidth, wallWidth * 2.5f + passageWidth * 2, wallHeight / 2, wallWidth * 1.5f + passageWidth * 1);
+        addWall(wallWidth * 4 + passageWidth * 3, wallHeight, wallWidth, wallWidth * 3 + passageWidth * 2.5f, wallHeight / 2, wallWidth * 2.5f + passageWidth * 2);
+        addWall(wallWidth * 3 + passageWidth * 2, wallHeight, wallWidth, wallWidth * 2.5f + passageWidth * 2, wallHeight / 2, wallWidth * 1.5f + passageWidth * 1);
+        addWall(wallWidth * 2 + passageWidth * 1, wallHeight, wallWidth, wallWidth * 1 + passageWidth * 0.5f, wallHeight / 2, wallWidth * 3.5f + passageWidth * 3);
+        addWall(wallWidth * 2 + passageWidth * 1, wallHeight, wallWidth, wallWidth * 3 + passageWidth * 2.5f, wallHeight / 2, wallWidth * 3.5f + passageWidth * 3);
+        addWall(wallWidth * 2 + passageWidth * 1, wallHeight, wallWidth, wallWidth * 2 + passageWidth * 1.5f, wallHeight / 2, wallWidth * 4.5f + passageWidth * 4);
+        addWall(wallWidth * 2 + passageWidth * 1, wallHeight, wallWidth, wallWidth * 4 + passageWidth * 3.5f, wallHeight / 2, wallWidth * 4.5f + passageWidth * 4);
+
+        // Adding floor
+        floor = new Floor(50, 0.1f, 50, assetManager, localRootNode, 0, -0.1f, 0, bulletAppState);
+
+        // Adding light
         DirectionalLight sun = new DirectionalLight();
         sun.setColor(ColorRGBA.White);
-        sun.setDirection(new Vector3f(10,10,10).normalizeLocal());
+        sun.setDirection(new Vector3f(10, 10, 10).normalizeLocal());
         localRootNode.addLight(sun);
+    }
+
+    private void addWall(float x, float y, float z, float px, float py, float pz) {
+        walls.add(new Wall(x, y, z, assetManager, localRootNode, px, py, pz, bulletAppState));
     }
 }
---
+++ src/main/java/labirinthan/MainCharacter.java
@@ -0,0 +1,98 @@
+package labirinthan;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.BetterCharacterControl;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+
+public class MainCharacter extends AbstractAppState implements ActionListener {
+
+    private SimpleApplication app;
+    private BulletAppState bulletAppState;
+    private BetterCharacterControl characterControl;
+    private Node characterNode;
+
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, forward = false, backward = false;
+
+    private final float CHARACTER_SPEED = 10f;
+    private final float JUMP_FORCE = 800f; // Adjust this value to make the jump stronger
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.app = (SimpleApplication) app;
+        bulletAppState = this.app.getStateManager().getState(BulletAppState.class);
+
+        characterNode = new Node("Character");
+        characterControl = new BetterCharacterControl(0.5f, 1.8f, 80f);
+        characterControl.setJumpForce(new Vector3f(0, JUMP_FORCE, 0));
+        characterNode.addControl(characterControl);
+        this.app.getRootNode().attachChild(characterNode);
+        bulletAppState.getPhysicsSpace().add(characterControl);
+
+        initKeys();
+    }
+
+    private void initKeys() {
+        this.app.getInputManager().addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        this.app.getInputManager().addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        this.app.getInputManager().addMapping("Forward", new KeyTrigger(KeyInput.KEY_W));
+        this.app.getInputManager().addMapping("Backward", new KeyTrigger(KeyInput.KEY_S));
+        this.app.getInputManager().addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+
+        this.app.getInputManager().addListener(this, "Left", "Right", "Forward", "Backward", "Jump");
+    }
+
+    @Override
+    public void onAction(String binding, boolean isPressed, float tpf) {
+        switch (binding) {
+            case "Left":
+                left = isPressed;
+                break;
+            case "Right":
+                right = isPressed;
+                break;
+            case "Forward":
+                forward = isPressed;
+                break;
+            case "Backward":
+                backward = isPressed;
+                break;
+            case "Jump":
+                if (isPressed) {
+                    characterControl.jump();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void update(float tpf) {
+        super.update(tpf);
+        Vector3f camDir = this.app.getCamera().getDirection().clone().setY(0).normalizeLocal().multLocal(CHARACTER_SPEED);
+        Vector3f camLeft = this.app.getCamera().getLeft().clone().setY(0).normalizeLocal().multLocal(CHARACTER_SPEED);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (forward) {
+            walkDirection.addLocal(camDir);
+        }
+        if (backward) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        characterControl.setWalkDirection(walkDirection);
+        this.app.getCamera().setLocation(characterNode.getLocalTranslation().add(0, 1.8f, 0));
+    }
+}
